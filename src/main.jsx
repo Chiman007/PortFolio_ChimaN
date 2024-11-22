c4fcd25504ebbbd7f162d9725afcfe2aae6dd442
Chiman007 <152136573+Chiman007@users.noreply.github.com>
2024-11-22 10:05:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import Content from './Content.jsx'
+import App from './App.jsx'
+// import Content from './Content.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Content/>
+    <App/>
   </React.StrictMode>,
 )
